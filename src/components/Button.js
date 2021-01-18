af4bd32517b70c53c281805b832e8811c5e2d2d6
Steven <schenn1992@gmail.com>
2021-01-18 19:45:17 +0000

    implement classNames library

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,16 +2,13 @@ import React from "react";
 
 import "components/Button.scss";
 
+const classnames = require('classnames');
+
 export default function Button(props) {
-   let buttonClass = "button";
-
-   if(props.confirm) {
-      buttonClass += " button--confirm";
-   }
-
-   if(props.danger) {
-      buttonClass += " button--danger";
-    }
+   const buttonClass = classnames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+    });
   
    return <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>{props.children}</button>;
 }
