81a5c7b21f091f593eb6c1b119f02c13086e1c46
Steven <schenn1992@gmail.com>
2021-01-20 14:04:43 +0000

    refactor Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,27 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button"
 
 export default function Form(props) {
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  
+  function reset() {
+    setName("");
+    setInterviewer(null);
+  }
+  
+  function cancel() {
+    reset();
+    props.onCancel();
+  }
+
+  function save() {
+    props.onSave(name, interviewer)
+  }
   
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            value={props.name}
+            value={name}
+            onChange={event => 
+              setName(event.target.value)
+            }
           />
-        </form>
-        <InterviewerList interviewers={props.interviewers} value={props.interviewer} onChange={props.setInterviewer} />
+        </form> 
+        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
-          <Button confirm onClick={props.onSave}>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={save}>Save</Button>
         </section>
       </section>
     </main>
