48a44ce8ac3a89a0b9902323733a4e80121ea517
Steven <schenn1992@gmail.com>
2021-01-20 04:04:23 +0000

    add Edit and Create story

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 import Button from "components/Button";
 
 import "index.scss";
@@ -157,3 +158,19 @@ storiesOf("Appointment", module)
   ))
   .add("Status", () => <Status message="Deleting"/>)
   .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")}/>)
+  .add("Edit", () => (
+    <Form 
+    name="Justin Beaver" 
+    interviewers={interviewers} 
+    interviewer={1} 
+    onSave={action("onSave")} 
+    onCancel={action("onCancel")}
+    /> 
+  ))
+  .add("Create", () => (
+    <Form 
+      interviewers={interviewers} 
+      onSave={action("onSave")} 
+      onCancel={action("onCancel")}
+    /> 
+  ))
