ae72fc1c0f85d3b20fa447d70eedd29132635b46
Steven Chen <schenn1992@gmail.com>
2021-01-21 17:01:20 -0500

    add getInterview function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,10 +1,21 @@
 export function getAppointmentsForDay(state, day) {
-  const currentDay = state.days.find(currentDay => currentDay.name === day);
+  const currentDay = state.days.find(eachDay => eachDay.name === day);
   if (!currentDay) {
     return [];
   }
-  const appointmentArray = currentDay.appointments.map((appointmentId) => state.appointments[appointmentId])
-  return appointmentArray;
-}
+  const appointmentsArray = currentDay.appointments.map((appointmentId) => state.appointments[appointmentId])
+  return appointmentsArray;
+};
 
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+  const interviewId = interview.interviewer;
+  
+  return {  
+    "student": interview.student,
+    "interviewer": state.interviewers[interviewId]
+  }; 
+}
 
