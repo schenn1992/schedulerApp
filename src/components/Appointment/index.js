ce83e02e85a30dcd7c2e3f30c898bf91b6130de0
Steven Chen <schenn1992@gmail.com>
2021-01-22 17:49:44 -0500

    apply onDelete function

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,7 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
 
 import "./styles.scss";
 
@@ -11,6 +12,7 @@ const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
+const DELETING = "DELETING";
 
 export default function Appointment(props) {
 
@@ -23,9 +25,17 @@ export default function Appointment(props) {
       student: name,
       interviewer
     };
-    
+    transition(SAVING)
     props.bookInterview(props.id, interview).then(() => {
       transition(SHOW)
+    })
+  };
+
+  function onDelete() {
+    transition(DELETING);
+    props.cancelInterview(props.id)
+    .then(() => {
+      transition(EMPTY)
     })
   };
   
@@ -37,6 +47,7 @@ export default function Appointment(props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+          onDelete={onDelete}
         />
       )}
       {mode === CREATE && (
@@ -46,7 +57,8 @@ export default function Appointment(props) {
           onSave={save}
         />
       )}
-      
+      {mode === SAVING && <Status  /> }
+      {mode === DELETING && <Status  message="Deleting" />}
     </article>
   )
 };
