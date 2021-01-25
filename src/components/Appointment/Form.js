506f37cace37e5ab989af569da1a3be0430f7d17
Steven Chen <schenn1992@gmail.com>
2021-01-25 18:37:19 -0500

    add data-testid as prop

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -33,6 +33,7 @@ export default function Form(props) {
             onChange={event => 
               setName(event.target.value)
             }
+            data-testid="student-name-input"
           />
         </form> 
         <InterviewerList 
