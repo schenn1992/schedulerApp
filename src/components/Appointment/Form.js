62733de99aa5fd9dccec952344440b9321d67757
Steven Chen <schenn1992@gmail.com>
2021-01-25 13:40:32 -0500

    reformat InterviewerList props

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -35,7 +35,11 @@ export default function Form(props) {
             }
           />
         </form> 
-        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
+        <InterviewerList 
+          interviewers={props.interviewers} 
+          value={interviewer} 
+          onChange={setInterviewer} 
+        />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
