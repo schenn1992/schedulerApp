baab683404809b3e1d24c29daa8ba7f4040b72bf
Steven Chen <schenn1992@gmail.com>
2021-01-26 17:22:17 -0500

    add axios put and delete

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -79,5 +79,20 @@ export default {
         data: fixtures.interviewers
       });
     }
+  }),
+  
+  put: jest.fn(() => {
+    return Promise.resolve({
+      status: 204,
+      statusText: "No Content"
+    });
+  }),
+
+  delete: jest.fn(() => {
+    return Promise.resolve({
+      status: 204,
+      statusText: "No content",
+    });
   })
+  
 }
