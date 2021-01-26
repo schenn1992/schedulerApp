825fe1b6010a6203c70943373f702dad28d44387
Steven Chen <schenn1992@gmail.com>
2021-01-26 17:24:33 -0500

    add data-testid=day to <li>

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -17,7 +17,11 @@ export default function DayListItem(props) {
   const dayClass = classnames("day-list__item", { "day-list__item--selected" : props.selected }, { "day-list__item--full" : props.spots === 0 })
   
   return (
-    <li onClick={() => props.setDay(props.name)} className={dayClass} >
+    <li  
+      data-testid="day" 
+      onClick={() => props.setDay(props.name)} 
+      className={dayClass} 
+    >
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
